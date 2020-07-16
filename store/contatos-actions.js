bbcc44e8fdc6adb327636cd36e1528a2016307b0
Guilherme Gaddini <ferreiragaddini@bol.com.br>
2020-07-16 14:24:28 -0300

    Show Data On Log

--- store/contatos-actions.js
+++ store/contatos-actions.js
@@ -2,6 +2,7 @@ export const ADD_CONTATO = 'ADD_CONTATO';
 
 export const addContato = (nome, telefone) => {
     console.log('Action Log | nome, telefone: ', nome, telefone);
+    console.log(nome, telefone);
     return {
         type: ADD_CONTATO, dadosContato: { nomeContato: nome, telefone: telefone }
     }
